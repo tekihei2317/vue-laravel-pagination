8b9063c77b99c0b288e0b856846e37e7e7878a11
tekihei2317 <tekihei2317@gmail.com>
2022-04-22 19:45:48 +0900

    feat: ページネーションをコンポーネント化

---
+++ frontend/components/custom/Pagination.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+type PaginationLink = {
+  url: string | null
+  label: string
+  active: boolean
+}
+
+type Props = {
+  links: PaginationLink[]
+  onClickLink: (url: string) => void
+}
+
+const { links, onClickLink } = defineProps<Props>()
+</script>
+
+<template>
+  <div class="pagination">
+    <template v-for="link in links">
+      <button
+        :disabled="link.url === null"
+        class="pagination-link"
+        :class="{
+          'pagination-link-enabled': link.url !== null,
+          'pagination-link-active': link.active,
+        }"
+        @click="onClickLink(link.url)"
+      >
+        {{ link.label }}
+      </button>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+.pagination {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.pagination-link {
+  margin-right: 4px;
+  padding: 12px 16px;
+  border: 1px solid gray;
+  border-radius: 4px;
+  background-color: white;
+}
+
+.pagination-link-active {
+  border: 1px solid blue;
+  color: blue;
+}
+
+.pagination-link-enabled:hover {
+  cursor: pointer;
+  background-color: lightgray;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -31,47 +31,16 @@ const onClickLink = (url) => {
         </tr>
       </tbody>
     </table>
-
-    <div class="pagination">
-      <template v-for="link in taskPaginator.meta.links">
-        <button
-          :disabled="link.url === null"
-          class="pagination-link"
-          :class="{
-            'pagination-link-enabled': link.url !== null,
-            'pagination-link-active': link.active,
-          }"
-          @click="onClickLink(link.url)"
-        >
-          {{ link.label }}
-        </button>
-      </template>
-    </div>
+    <custom-pagination
+      :links="taskPaginator.meta.links"
+      :on-click-link="onClickLink"
+      class="task-pagination"
+    />
   </div>
 </template>
 
-<style scoped>
-.pagination {
+<style>
+.task-pagination {
   margin-top: 16px;
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.pagination-link {
-  margin-right: 4px;
-  padding: 12px 16px;
-  border: 1px solid gray;
-  border-radius: 4px;
-  background-color: white;
-}
-
-.pagination-link-active {
-  border: 1px solid blue;
-  color: blue;
-}
-
-.pagination-link-enabled:hover {
-  cursor: pointer;
-  background-color: lightgray;
 }
 </style>
